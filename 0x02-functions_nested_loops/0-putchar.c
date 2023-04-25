82a97d985e50f38c78a77baf3e2649e81c9fc61a
Melvyne-Adisa <melvynea@gmail.com>
2023-04-25 13:45:46 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
-	/**
-	 * main - Entry point
-	 * Description: prints _putchat to stdout
-	 * Return: 0
-	 */
+/**
+ * main - Entry point
+ * Description: prints _putchat to stdout
+ * Return: 0
+ */
 
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
