b1d49690c552eacb8c91fcb1d76972eb6b6ad698
Melvyne-Adisa <melvynea@gmail.com>
2023-04-26 04:20:36 -0700

    task eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 {
 	int i, j;
-	
+
 	if (n <= 98)
 	{
 	for (i = n; i <= 98; i++)
@@ -28,6 +28,7 @@ void print_to_98(int n)
 	{
 	if (j != 98)
 	printf("%d, ", j);
+	
 	else if (j == 98)
 	printf("%d\n", j);
 	}
