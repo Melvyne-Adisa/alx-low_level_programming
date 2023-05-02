b9a08620792dab60655a2caf71833a5fbc71cc80
Melvyne-Adisa <melvynea@gmail.com>
2023-05-02 11:41:51 -0700

    task four

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,12 @@ void print_rev(char *s)
 
 	int longi = 0;
 
-	int 0;
+	int o;
 
 	while (*s != '\0')
 	{
 	longi++;
-	s++
+	s++;
 	}
 	s--;
 	for (o = longi; o > 0; o--)
@@ -26,6 +26,6 @@ void print_rev(char *s)
 	s--;
 	}
 
-	_putchat('\n');
+	_putchar('\n');
 
 }
